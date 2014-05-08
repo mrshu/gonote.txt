b496b18b55a7f01c356af2e153f20bd16f05195b
mr.Shu <mr@shu.io>
2014-05-09 00:25:16 +0200

    A bit more structure

--- gonoter.go
+++ gonoter.go
@@ -5,11 +5,27 @@ import  (
         "os"
         "github.com/spf13/cobra"
         "github.com/rakyll/globalconf"
+        "flag"
 )
 
 func main() {
 
         conf, _ := globalconf.New("gonoter")
+
+        var flagNotedir = flag.String("dir", "", "Location of the note.txt directory.")
+        var dir string
+
+        var cmdAdd = &cobra.Command{
+            Use:   "add [title] [category]",
+            Short: "Add a note.",
+            Long:  `Add a note into a category.`,
+            Run: func(cmd *cobra.Command, args []string) {
+                if len(args) < 1 {
+                        fmt.Println("I need something to add")
+                        return
+                }
+            },
+        }
 
         var GonoterCmd = &cobra.Command{
             Use:   "gonoter",
@@ -18,4 +34,19 @@ func main() {
             Run: func(cmd *cobra.Command, args []string) {
             },
         }
+
+        GonoterCmd.PersistentFlags().StringVarP(&dir, "directory", "", "",
+                                     "Location of the note.txt directory.")
+
+        if dir == "" {
+                if *flagNotedir == "" {
+                        dir = "~/notes"
+                } else {
+                        dir = *flagNotedir
+                }
+        }
+        conf.ParseAll()
+
+        GonoterCmd.AddCommand(cmdAdd)
+        GonoterCmd.Execute()
 }
